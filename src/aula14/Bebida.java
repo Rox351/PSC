5883e55c90f7e73a09a8fe37cd95930d018d0322
Leonardo Soares <95651574+Rox351@users.noreply.github.com>
2023-03-31 20:12:09 -0300

    31/03/2023

---
+++ src/aula14/Bebida.java
@@ -0,0 +1,11 @@
+package aula14;
+
+public class Bebida extends Caloria {
+
+	public Bebida(String descricao, int qtdeCalorias) {
+		super(descricao, qtdeCalorias);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
